ab6768297146c209d805edc8fe5fcd1b77a3be21
c17hawke <2016peb5126@mnit.ac.in>
2021-03-16 21:20:26 +0530

    read config

--- src/pipeline_01_data_preparation.py
+++ src/pipeline_01_data_preparation.py
@@ -4,15 +4,22 @@ import yaml
 import logging
 
 
+def read_params(config_path):
+    with open(config_path) as yaml_file:
+        config = yaml.safe_load(yaml_file)
+    return config
 
-
-
+def main(config_path, datasource):
+    config = read_params(config_path)
+    print(config)
+    
 if __name__=="__main__":
     args = argparse.ArgumentParser()
-    args.add_argument("--config", default="default")
+    default_config_path = os.path.join("config", "params.yaml")
+    args.add_argument("--config", default=default_config_path)
     args.add_argument("--datasource", default=None)
 
     parsed_args = args.parse_args()
-    print(parsed_args.config, parsed_args.datasource)
+    main(config_path=parsed_args.config , datasource=parsed_args.datasource)
 
     
