58198b8b52d128e5423dd5f102bb9cf3cca40f95
c17hawke <2016peb5126@mnit.ac.in>
2021-03-16 19:43:59 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='its a wafer project using mlops',
+    author='c17hawke',
+    license='MIT',
+)
